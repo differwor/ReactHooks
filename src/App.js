c17a96d8d65bbea25454aab46ff25fb8499be1cc
Phuc <phxc29@gmail.com>
2021-08-13 22:11:40 +0700

    todolist

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
+import { useState } from 'react';
 import './App.scss';
 import ColorBox from './components/ColorBox';
+import TodoList from './components/TodoList';
 
 function App() {
+
+  const [todoList, setTodoList] = useState([
+    { id: 1, title: 'I love you'},
+    { id: 2, title: 'He love you'},
+    { id: 3, title: 'They love you'}
+  ]);
+
+  function handleTodoClick(todo) {
+    const index = todoList.findIndex(x => x.id === todo.id);
+    if (index < 0) return; 
+
+    const newTodoList = [ ...todoList ]; // tao list moi bang todolist
+    newTodoList.splice(index, 1); // xoa 1 phan tu o vi tri index
+    setTodoList(newTodoList);
+  }
+
   return (
     <div className="app">
-      <h1>React hooks color box</h1>
-
+      <h1>React hooks - TodoList</h1>
+      
       <ColorBox />
+      <TodoList todos={todoList} onToDoClick={handleTodoClick} />
     </div>
   );
 }
---
+++ src/components/TodoList/index.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+TodoList.propTypes = {
+    todos: PropTypes.array,
+    onToDoClick: PropTypes.func,
+};
+
+TodoList.defaultProps = {
+    todos: [],
+    onToDoClick: null,
+}
+
+
+function TodoList(props) {
+    const {todos, onToDoClick} = props; // su dung obj destructoring de lay props
+
+    function handleClick(todo) {
+        if (onToDoClick) {
+            onToDoClick(todo);
+        }
+    }
+
+    return (
+        <ul className="todo-list">
+            {todos.map(todo => (
+                <li 
+                    key={todo.id}
+                    onClick={() => handleClick(todo)}
+                >
+                    {todo.title}
+                </li>
+            ))}
+        </ul>
+    );
+}
+
+
+export default TodoList;
