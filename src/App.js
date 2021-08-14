883a735498fb0e1de54529f88a8ae3c1d1743135
Phuc <phxc29@gmail.com>
2021-08-14 17:01:17 +0700

    PostList (API fetch useEffect())

--- src/App.js
+++ src/App.js
@@ -1,16 +1,41 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.scss';
 import ColorBox from './components/ColorBox';
+import PostList from './components/PostList';
 import TodoForm from './components/TodoForm';
 import TodoList from './components/TodoList';
 
 function App() {
 
-  const [todoList, setTodoList] = useState([
+  const [todoList, setTodoList] = useState([ //tạo 1 state 
     { id: 1, title: 'I love you'},
     { id: 2, title: 'He love you'},
     { id: 3, title: 'They love you'}
   ]);
+
+  const [postlist, setPostList] = useState([]);
+
+  useEffect(() => {
+
+    // goi api bang lib Fetch
+    async function fetchPostList() {
+
+      try {
+        const requestUrl = 'http://js-post-api.herokuapp.com/api/posts?_limit=10&_page=1';
+      const response = await fetch(requestUrl);
+      const responseJSON = await response.json();
+      console.log({responseJSON});
+
+      const {data} = responseJSON;
+      setPostList(data);
+      } catch (error) {
+        console.log('Lỗi rồi kìa :3')
+      }
+      
+    }
+
+    fetchPostList();
+  }, []);
 
   function handleTodoClick(todo) {
     const index = todoList.findIndex(x => x.id === todo.id);
@@ -36,12 +61,13 @@ function App() {
 
   return (
     <div className="app">
-      <h1>React hooks - TodoList</h1>
+      <h1>React hooks - ColorBox - TodoList - TodoForm - PostList</h1>
       
-      <ColorBox />
-
+      {/* <ColorBox />
       <TodoForm onSubmit={handleTodoFormSubmit} />
-      <TodoList todos={todoList} onToDoClick={handleTodoClick} />
+      <TodoList todos={todoList} onToDoClick={handleTodoClick} /> */}
+
+      <PostList posts={postlist} />
       
     </div>
   );
